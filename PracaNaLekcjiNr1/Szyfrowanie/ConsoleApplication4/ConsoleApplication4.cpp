3d8af483c117b9fb6fed31474299cf4dd754991a
Gracjan Rozalski <gracjan.rozalski96@o2.pl>
2021-11-09 14:25:00 +0100

    Dodałem klasy do szyfrowania

---
+++ PracaNaLekcjiNr1/Szyfrowanie/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include "szyfry.h"
+using namespace std;
+
+int main()
+{
+	szyfry zmienna;
+	string trzecie, trzecie1;
+	string przestawie;
+	string cezar;
+	int odp;
+	string ciag;
+	cout << endl << "Podaj ciag znakow z malych liter: " << endl;
+	getline(cin, ciag);
+	cout << endl << "Co chcesz robic?" << endl;
+	cout << endl << "1.Szyfr podstawieniowy" << endl;
+	cout << endl << "2.Szyfr przestawieniowy" << endl;
+	cout << endl << "3.Szyfr podstawieniowy i przestawieniowy" << endl;
+	cout << endl << "4.Odszyfruj tekst" << endl;
+	cin >> odp;
+	if (odp == 1)
+	{
+		cezar = zmienna.podstawieniowy(ciag);
+		cout << endl << cezar << endl;
+	}
+	else if (odp == 2)
+	{
+		przestawie = zmienna.przestawieniowy(ciag);
+		cout << endl << przestawie << endl;
+
+	}
+	else if (odp == 3)
+	{
+		trzecie = zmienna.podstawieniowy(ciag);
+		trzecie = zmienna.przestawieniowy(trzecie);
+		cout << trzecie;
+
+	}
+	else if (odp == 4)
+	{
+		cout << endl << "Ktorys z tych ciagow jest poprawny" << endl;
+		cout << zmienna.odszyfrowanie(ciag);
+	}
+}
---
+++ PracaNaLekcjiNr1/Szyfrowanie/ConsoleApplication4/szyfry.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+class szyfry
+{
+public:
+	string podstawieniowy(string ciag1);
+	string przestawieniowy(string ciag2);
+	string odszyfrowanie(string ciag3);
+
+};
+
+
+
+
